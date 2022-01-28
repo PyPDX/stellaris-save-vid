a69bed2638b788daf506963312070ea26f59d43b
Michael Kim <mkim0407@gmail.com>
2022-01-28 02:19:21 -0800

    add starbase compatibility

--- static/js/game/gamestate.js
+++ static/js/game/gamestate.js
@@ -4,7 +4,7 @@ class Gamestate {
 
         this.stars = BaseIdMapObject.loadDict(Star, this._data.galactic_object, this);
         this.empires = BaseIdMapObject.loadDict(Empire, this._data.country, this);
-        this.starbases = BaseIdMapObject.loadDict(Starbase, this._data.starbases, this);
+        this.starbases = BaseIdMapObject.loadDict(Starbase, this._data.starbases || this._data.starbase_mgr.starbases, this);
         this.bypasses = BaseIdMapObject.loadDict(Bypass, this._data.bypasses, this);
         this.wormholes = BaseIdMapObject.loadDict(Wormhole, this._data.natural_wormholes, this);
 
